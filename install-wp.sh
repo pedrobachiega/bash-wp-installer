8de012d2130b668c5ab99e490f65c461ffd4d23d
Pedro Bachiega <pedro@pedrobachiega.com>
2012-04-16 11:02:14 -0300

    downloading common plugins

--- install-wp.sh
+++ install-wp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TODAY=$(date +%Y%m%d)
+SCRIPT_PATH=$(pwd)
 echo "==== $TODAY - Starting Wordpress installation proccess ===="
 
 echo -e "Path installation (like '/home/user/'): "
@@ -9,14 +10,15 @@ echo "Moving to $wp_path"
 cd $wp_path
 
 echo "Downloading latest version of Wordpress"
-curl -O http://wordpress.org/latest.zip
-unzip latest.zip
+curl --silent -O http://wordpress.org/latest.zip
+unzip -q latest.zip
 rm latest.zip
 echo "Moving into Wordpres folder"
 cd wordpress/
 
 echo "Downloading config file"
-curl -O http://res.pedrobachiega.com/wordpress/wp-config.php
+curl --silent -O http://res.pedrobachiega.com/wordpress/wp-config.php
+#TODO
 #salt=$(curl "https://api.wordpress.org/secret-key/1.1/salt/")
 #sed_expression="s/\/\/ Authentication Unique Keys and Salts/'$salt'/g"
 #sed -e "$sed_expression" wp-config.php
@@ -34,4 +36,15 @@ read wp_db_host
 sed -i.bkp "s/localhost/$wp_db_host/g" wp-config.php
 wp_db_prefix=$[ ( $RANDOM % 999 )  + 1 ]_
 sed -i.bkp "s/wp_xpto_/wp_$wp_db_prefix/g" wp-config.php
+
+#TODO - install wp with blog name, admin user and initial configs
+
+echo "Downloading common plugins"
+cd wp-content/plugins/
+while read LINE; do
+  curl --silent -O "$LINE"
+  file=$(echo $LINE | sed "s/http:\/\/downloads.wordpress.org\/plugin\///g")
+  unzip -qo $file
+  rm $file
+done < $SCRIPT_PATH/plugins-list.txt
 
